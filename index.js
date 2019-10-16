09ec087008d94ce0d1a7f76d6dd39ae6af81b91a
evil <40669563+evilpauwse1@users.noreply.github.com>
2019-10-15 23:06:36 -0400

    Update for start testing

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-exports.test = function(input) {
-    return input;
+exports.testPage = function(authToken,twitterSes,link) {
+    let pup = require("pupeteer");
+    let aT = authToken;
+    let tS = twitterSes;
+    (async () => {
+        const browser = await pup.launch({headless: true, args:['--no-sandbox']});
+        const page = await browser.newPage();
+        await page.setCookie({name:"auth_token",value:aT,domain:".twitter.com"},{name:"_twitter_sess",value:tS,domain:".twitter.com"});
+        await page.goto(link);
+        return page.response;
+    })
 }
